a1cf7d0bc73e4668ccb8227e17e6a6f004972295
Srdjan Djenader <djenka76@gmail.com>
2021-07-19 20:17:35 -0500

    Home.js converted to hook component

--- app/components/screens/Home.js
+++ app/components/screens/Home.js
@@ -5,10 +5,52 @@ import {
   TouchableOpacity,
   TextInput,
 } from 'react-native';
-import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
 import * as appActions from '../../redux/actions';
+
+const Home = () => {
+  const [owner, setOwner] = useState('');
+  const [repo, setRepo] = useState('');
+  const [numberOfCommits, setNumberOfCommits] = useState(null);
+  const dispatch = useDispatch();
+
+  return (
+    <View style={styles.mainContainer}>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => setOwner(text)}
+        placeholder="Owner"
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={text => setRepo(text)}
+        placeholder="Repo"
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={num => setNumberOfCommits(num)}
+        placeholder="Number of Commits"
+        keyboardType="numeric"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          dispatch(
+            appActions.navigateTo('CommitList', {
+              owner,
+              repo,
+              numberOfCommits,
+            }),
+          );
+        }}>
+        <Text style={styles.text}>Get Commits</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Home;
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -36,73 +78,3 @@ const styles = StyleSheet.create({
     borderWidth: 1,
   },
 });
-
-@connect(
-  () => ({}),
-  dispatch => ({actions: bindActionCreators(appActions, dispatch)}),
-)
-class Home extends React.Component {
-  static navigationOptions = () => ({
-    title: 'Welcome!',
-  });
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      owner: '',
-      repo: '',
-      numberOfCommits: 0,
-    };
-  }
-
-  render() {
-    const {actions} = this.props;
-    const {owner, repo, numberOfCommits} = this.state;
-    return (
-      <View style={styles.mainContainer}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={text =>
-            this.setState({
-              owner: text,
-            })
-          }
-          placeholder="Owner"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={text =>
-            this.setState({
-              repo: text,
-            })
-          }
-          placeholder="Repo"
-        />
-        <TextInput
-          style={styles.textInput}
-          onChangeText={num =>
-            this.setState({
-              numberOfCommits: num,
-            })
-          }
-          placeholder="Number of Commits"
-          keyboardType="numeric"
-        />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            actions.navigateTo('CommitList', {
-              owner,
-              repo,
-              numberOfCommits,
-            });
-          }}>
-          <Text style={styles.text}>Get Commits</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
-
-export default Home;
