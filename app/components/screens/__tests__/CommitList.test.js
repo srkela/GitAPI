153cfa24fc68b50d44e723e15e9bcabcfc389d5b
Srdjan Djenader <djenka76@gmail.com>
2021-07-24 12:46:30 -0500

    fixed CommitList test code cleanup

--- app/components/screens/__tests__/CommitList.test.js
+++ app/components/screens/__tests__/CommitList.test.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
-import _ from 'lodash';
+import {Provider} from 'react-redux';
 import CommitList from '../CommitList';
 import store from '../../../redux/store';
-import mockCommitList from '../../../../mocks/mockCommitList';
 
-jest.mock('../../../redux/store', () => ({
-  getState: jest.fn(),
-}));
+const getParamMock = jest
+  .fn()
+  .mockReturnValueOnce('srkela')
+  .mockReturnValueOnce('GitAPI')
+  .mockReturnValueOnce(25);
 
-let mockState;
-store.getState = () => mockState;
-
-const initialState = {
-  commitsList: mockCommitList.commits.commitsList,
-};
-
-beforeEach(() => {
-  mockState = _.cloneDeep(initialState);
-});
-afterEach(() => {
-  mockState = _.cloneDeep(initialState);
-});
 describe('CommitList', () => {
-  it('should render', () => {
-    render(<CommitList commitsList={initialState} />);
+  test('should render', () => {
+    const navigation = {
+      getParam: getParamMock,
+    };
+    render(
+      <Provider store={store}>
+        <CommitList
+          navigation={navigation}
+          // commitsList={initialState.commitsList}
+          actions={{getCommitList: jest.fn()}}
+        />
+      </Provider>,
+    );
   });
 });
